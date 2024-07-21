5b97c314626226004e700b2b8c9890a02956eea2
stnokott <30934296+stnokott@users.noreply.github.com>
2024-07-21 21:36:11 +0200

    chore(dev): split up protobuf definitions

--- internal/proto/proto.go
+++ internal/proto/proto.go
@@ -3,4 +3,4 @@ package proto
 
 //go:generate go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.34.2
 //go:generate go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.4.0
-//go:generate protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative server.proto
+//go:generate protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative *.proto
