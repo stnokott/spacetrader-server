8b67328edab9e8e06236bbd2b24a0b5e337ccdd8
stnokott <30934296+stnokott@users.noreply.github.com>
2024-11-15 10:50:41 +0100

    fix: jump gate cache creation

--- internal/db/schema/000003_create_jump_gates_table.up.sql
+++ internal/db/schema/000003_create_jump_gates_table.up.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS jump_gates (
 	waypoint TEXT NOT NULL,
 	connects_to TEXT NOT NULL,
 	FOREIGN KEY(waypoint) REFERENCES waypoints(symbol),
-	FOREIGN KEY(connects_to) REFERENCES waypoints(symbol)
+	FOREIGN KEY(connects_to) REFERENCES waypoints(symbol) DEFERRABLE INITIALLY DEFERRED -- defer since waypoint might not exist yet (TODO: refactor system cache creation so that waypoints can be created on-demand and we dont need this anymore)
 );
 
 CREATE INDEX IF NOT EXISTS jump_gate_waypoint_index ON jump_gates(waypoint);
